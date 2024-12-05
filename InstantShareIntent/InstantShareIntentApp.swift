fda8b5656664917725e92a1a196c086a4b3bae2d
Claudin <claudin@smoose.in>
2024-12-05 11:00:10 +0530

    Initial Commit

---
+++ InstantShareIntent/InstantShareIntentApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstantShareIntentApp.swift
+//  InstantShareIntent
+//
+//  Created by Nicky Alfred on 05/12/24.
+//
+
+import SwiftUI
+
+@main
+struct InstantShareIntentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InstantShareIntentTests/InstantShareIntentTests.swift
@@ -0,0 +1,17 @@
+//
+//  InstantShareIntentTests.swift
+//  InstantShareIntentTests
+//
+//  Created by Nicky Alfred on 05/12/24.
+//
+
+import Testing
+@testable import InstantShareIntent
+
+struct InstantShareIntentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
